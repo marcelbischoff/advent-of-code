ff6b6c1f729bd9945081e05eedca69adf3bc774c
Marcel Bischoff <mrclbschff@gmail.com>
2023-12-03 15:34:15 -0500

    Option char

--- 2023/day03/bin/main.ml
+++ 2023/day03/bin/main.ml
@@ -2,21 +2,21 @@ let read_file file = In_channel.with_open_text file In_channel.input_all
 let split_str sep = Str.(split (regexp sep))
 let sum = List.fold_left ( + ) 0
 let prod = List.fold_left ( * ) 1
-let string_of_char = String.make 1  
+let string_of_char = String.make 1
+(*let (let* ) = Option.bind *)
 
-let get_from_mat mat h w i j =
-  let () = assert (j >= 0 && j < w) in
-  let () = assert (i >= 0 && i < h) in
-  String.get mat (j + i*w) |> string_of_char
+let get_from_mat arr h w i j =
+  if (j >= 0 && j < w && i >= 0 && i < h) then
+    Some (String.get arr (j + (i * w)))
+  else None
 
-let scan mat h w =
-  let char_of_string s = String.get s 0 in
-  let get = get_from_mat mat h w in
+let scan arr h w =
+  let get = get_from_mat arr h w in
   let _foo = get 9 2 in
   let is_marker i j =
     if i < 0 || i >= h || j < 0 || j >= w then false
     else
-      match get i j |> char_of_string with
+      match get i j |> Option.get with
       | '0' .. '9' | '.' -> false
       | _ -> true
   in
@@ -37,10 +37,10 @@ let scan mat h w =
     if i >= h then acc
     else if j >= w then aux (i + 1) 0 false "" new_acc
     else
-      let item = get i j in
-      match char_of_string item with
+      let item = get i j |> Option.get in
+      match item with
       | '0' .. '9' ->
-          aux i (j + 1) (is_part_number || check i j) (part_number ^ item) acc
+          aux i (j + 1) (is_part_number || check i j) (part_number ^ (string_of_char item)) acc
       | _ -> aux i (j + 1) false "" new_acc
   in
   aux 0 0 false "" 0
@@ -49,12 +49,12 @@ let solve file scan =
   let lines = read_file file |> split_str "\n" in
   let h = List.length lines in
   let w = List.nth lines 0 |> String.length in
-  let array = lines |> String.concat "" in
-  scan array h w
+  let arr = lines |> String.concat "" in
+  scan arr h w
 
 (* -- part 2 -- *)
 
-let scan2 mat h w =
+let scan2 arr h w =
   let comp a b =
     if a = b then 0
     else if fst a > fst b then 1
@@ -63,12 +63,10 @@ let scan2 mat h w =
     else -1
   in
   let unique = List.sort_uniq comp in
-  let char_of_string s = String.get s 0 in
-  let get = get_from_mat mat h w in
-  let _foo = get 9 2 in
+  let get= get_from_mat arr h w in
   let get_star i j =
     if i < 0 || i >= h || j < 0 || j >= w then None
-    else match get i j |> char_of_string with '*' -> Some (i, j) | _ -> None
+    else match get i j |> Option.get with '*' -> Some (i, j) | _ -> None
   in
   let getstars i j =
     [
@@ -91,12 +89,12 @@ let scan2 mat h w =
     if i >= h then acc
     else if j >= w then aux (i + 1) 0 [] "" new_acc
     else
-      let item = get i j in
-      match char_of_string item with
+      let item = get i j |> Option.get in
+      match item with
       | '0' .. '9' ->
           aux i (j + 1)
             (List.concat [ stars; getstars i j ])
-            (part_number ^ item) acc
+            (part_number ^ (string_of_char item)) acc
       | _ -> aux i (j + 1) [] "" new_acc
   in
   let part_numbers = aux 0 0 [] "" [] in
