524bd1d513945fbf732d8817085eda14ba2e0a2f
Marcel Bischoff <mrclbschff@gmail.com>
2024-12-01 22:43:07 -0600

    refactor

--- 2024/day01/bin/main.ml
+++ 2024/day01/bin/main.ml
@@ -2,20 +2,10 @@ let read_file file = In_channel.with_open_text file In_channel.input_all
 let split_str sep = Str.(split (regexp sep))
 let sum = List.fold_left ( + ) 0
 
-let solve filename =
-  let data =
-    read_file filename |> split_str "\n"
-    |> List.map (split_str " ")
-    |> List.map (List.filter_map int_of_string_opt)
-  in
-  let extract_sort data n =
-    data |> List.filter_map (fun x -> List.nth_opt x n) |> List.sort compare
-  in
-  List.combine (extract_sort data 0) (extract_sort data 1)
-  |> List.map (fun (x, y) -> x - y |> abs)
-  |> sum
+let conpute_part1 first second =
+  List.combine first second |> List.map (fun (x, y) -> x - y |> abs) |> sum
 
-let solve2 filename =
+let solve filename func =
   let data =
     read_file filename |> split_str "\n"
     |> List.map (split_str " ")
@@ -24,13 +14,14 @@ let solve2 filename =
   let extract_sort data n =
     data |> List.filter_map (fun x -> List.nth_opt x n) |> List.sort compare
   in
-  let mult i =
-    extract_sort data 1 |> List.map (fun x -> if x == i then 1 else 0) |> sum
-  in
-  extract_sort data 0 |> List.map (fun x -> x * mult x) |> sum
+  func (extract_sort data 0) (extract_sort data 1)
+
+let conpute_part2 first second =
+  let mult i = second |> List.map (fun x -> if x == i then 1 else 0) |> sum in
+  first |> List.map (fun x -> x * mult x) |> sum
 ;;
 
-let () = assert (solve "sample.txt" = 11) in
-let () = assert (solve2 "sample.txt" = 31) in
-let () = solve "input.txt" |> string_of_int |> print_endline in
-solve2 "input.txt" |> string_of_int |> print_endline
+let () = assert (solve "sample.txt" conpute_part1 = 11) in
+let () = assert (solve "sample.txt" conpute_part2 = 31) in
+let () = solve "input.txt" conpute_part1 |> string_of_int |> print_endline in
+solve "input.txt" conpute_part2 |> string_of_int |> print_endline
