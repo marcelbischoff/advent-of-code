be6439cfe5fa0c1e84f41112d7b4936a7f8effa4
Marcel Bischoff <mrclbschff@gmail.com>
2024-12-03 22:28:04 -0600

    part2

--- 2024/day02/bin/main.ml
+++ 2024/day02/bin/main.ml
@@ -21,6 +21,16 @@ let is_safe lst =
   | h :: w :: t ->
       if h < w then check_inc (h :: w :: t) else check_des (h :: w :: t)
 
+let is_safe2 lst =
+  let rec remove_one head tail =
+    match (tail, head) with
+    | [], _ -> 0
+    | h :: new_tail, head ->
+        if is_safe (List.concat [ head; new_tail ]) > 0 then 1
+        else remove_one (List.concat [ head; [ h ] ]) new_tail
+  in
+  if is_safe lst = 1 then 1 else remove_one [] lst
+
 let solve filename func =
   let data =
     read_file filename |> split_str "\n"
@@ -31,5 +41,7 @@ let solve filename func =
 ;;
 
 let () = assert (solve "sample.txt" is_safe = 2) in
+let () = assert (solve "sample.txt" is_safe2 = 4) in
 let () = solve "input.txt" is_safe |> string_of_int |> print_endline in
+let () = solve "input.txt" is_safe2 |> string_of_int |> print_endline in
 print_endline ""
