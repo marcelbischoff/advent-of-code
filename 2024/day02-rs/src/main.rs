8a043fc11bdfa47345637dc09e6f297c09789423
Marcel Bischoff <mrclbschff@gmail.com>
2024-12-16 21:52:00 -0600

    init

---
+++ 2024/day02-rs/src/main.rs
@@ -0,0 +1,74 @@
+use std::fs;
+
+fn parse_lines(input: &str) -> Vec<Vec<i32>> {
+    input
+        .lines()
+        .map(|s| {
+            s.split_whitespace()
+                .map(|t| t.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .collect::<Vec<Vec<i32>>>()
+}
+
+enum State {
+    Undetermined,
+    Inc,
+    Dec,
+    NotSafe,
+}
+fn check(a: i32, b: i32) -> bool {
+    (b > a) && (b < a + 4)
+}
+
+fn first_state(previous: i32, current: i32) -> State {
+    if (previous < current) && check(previous, current) {
+        return State::Inc;
+    }
+    if (current < previous) && check(current, previous) {
+        return State::Dec;
+    }
+    State::NotSafe
+}
+fn is_safe(line: Vec<i32>) -> i32 {
+    let (_, res) = line.into_iter().fold(
+        (None, State::Undetermined),
+        |(prior, state), current: i32| match (prior, state) {
+            (None, State::Undetermined) => (Some(current), State::Undetermined),
+            (Some(previous), State::Undetermined) => {
+                (Some(current), first_state(previous, current))
+            }
+            (Some(previous), State::Inc) => {
+                if check(previous, current) {
+                    (Some(current), State::Inc)
+                } else {
+                    (None, State::NotSafe)
+                }
+            }
+            (Some(previous), State::Dec) => {
+                if check(current, previous) {
+                    (Some(current), State::Dec)
+                } else {
+                    (None, State::NotSafe)
+                }
+            }
+            _ => (None, State::NotSafe),
+        },
+    );
+    match res {
+        State::Inc => 1,
+        State::Dec => 1,
+        _ => 0,
+    }
+}
+fn main() {
+    let sample = fs::read_to_string("../day02/sample.txt").expect("file not found");
+    let lines: Vec<Vec<i32>> = parse_lines(&sample);
+    assert!(lines.into_iter().map(is_safe).into_iter().sum::<i32>() == 2);
+
+    let sample = fs::read_to_string("../day02/input.txt").expect("file not found");
+    let lines: Vec<Vec<i32>> = parse_lines(&sample);
+    let result: i32 = lines.into_iter().map(is_safe).into_iter().sum();
+    assert!(result == 524);
+    println!("result: {result}");
+}
